11ffbb14eb64c97a456d7a984f08867925d72076
Dona0w0 <70440668+Dona0w0@users.noreply.github.com>
2021-10-15 23:44:00 -0500

    final

--- src/App.js
+++ src/App.js
@@ -4,33 +4,34 @@ import './App.css';
 function App() {
 
   const [nombre,setNombre] = useState(() =>{
-    const storage = localStorage.getItem('nombre');
-    const valInicial = JSON.parse(storage);
-    return valInicial || '';
+    const storage = localStorage.getItem('nombre'); //guarda en storage el contenido de input
+    const valInicial = JSON.parse(storage);//regresa el contenido de storage
+    return valInicial || '';//regresa el contenido guardado en la memoria cuando se recarge la pagina
+                            //o un vacio en caso de no tener contenido
   });
-  
-  const nombreRef = useRef();
 
-  const handleChange = () =>{
-    setNombre(nombreRef.current.value);
-  }
+  const nombreRef = useRef();
 
   useEffect(()=>{
     localStorage.setItem('nombre',JSON.stringify(nombre))
-  },[nombre]);
+  },[nombre]); 
+
+  function handleChange() {
+    setNombre(nombreRef.current.value); //cambia el condenido de nombre conforme se va escribiendo en el input 
+  }
+
 
 
   return (
     <div className="App">
-        <h1></h1>
-            <form>
-                <label>Nombre:</label> 
+        <h1>Guardemos tu nombre en el localStorage!</h1>
+            <div className="inputSpace">
+                <label>Nombre: </label> 
                 <input type="text" id="name" ref={nombreRef} onChange={handleChange}></input>
-           </form>
-    
+            </div>
 
       <React.Fragment>
-        <p>Hola <i>{nombre}</i>. ¡Bienvenido! </p>
+        <p>Hola <i>{nombre}</i>. ¡Bienvenid@! </p>
       </React.Fragment>
 
     </div>
